356d0628f13fdc6c4c2938e20e77a93f4521399d
yael zerbib <yaelzerbib@MacBook-de-yael.local>
2020-02-25 12:18:31 +0200

    Updates

---
+++ precache-manifest.5c78846b261c123d24b56e10626890c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000733cdbc325fa93a47d3e0d9f2dfb1",
+    "url": "/robotsfriends/index.html"
+  },
+  {
+    "revision": "8fc069d8c7aaa0440189",
+    "url": "/robotsfriends/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "f1c6a5e615fd78e6cd60",
+    "url": "/robotsfriends/static/css/main.1c24e225.chunk.css"
+  },
+  {
+    "revision": "8fc069d8c7aaa0440189",
+    "url": "/robotsfriends/static/js/2.16c3ee21.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/robotsfriends/static/js/2.16c3ee21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1c6a5e615fd78e6cd60",
+    "url": "/robotsfriends/static/js/main.01847c7b.chunk.js"
+  },
+  {
+    "revision": "1c17a9c77d43da6b1df9",
+    "url": "/robotsfriends/static/js/runtime-main.2f8780bf.js"
+  }
+]);
